4c6dfdb680161d1ad8a4ca0dd8459ff1fdd498f9
sangminlee98 <tkdals0975@naver.com>
2022-05-31 22:40:24 +0900

    react-router, 폴더구조

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,10 +1,16 @@
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      핫 리로딩
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
