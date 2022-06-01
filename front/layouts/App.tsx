a5c1c36ed705fb5005dcea8d14d59945cd3095a2
sangminlee98 <tkdals0975@naver.com>
2022-06-01 19:18:19 +0900

    redirect / to /login

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,12 +1,13 @@
 import Login from '@pages/Login';
 import SignUp from '@pages/SignUp';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate replace to='/login'/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<SignUp/>}/>
       </Routes>
