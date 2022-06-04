fc5f8f65eb7cec8904efe8dcc8731c2a1ac8469e
sangminlee98 <tkdals0975@naver.com>
2022-06-04 18:06:44 +0900

    회원가입 & 로그인페이지

--- front/pages/Login/index.tsx
+++ front/pages/Login/index.tsx
@@ -1,53 +1,47 @@
-import React from 'react';
+import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from '@pages/SignUp/styles';
+import React, { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {Success, Form, Error, Label, Header, Input, Button, LinkContainer} from './styles';
 
+const LogIn = () => {
+  const [logInError, setLogInError] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onChangeEmail = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  }, []);
+  const onChangePassword = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }, []);
+  const onSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email, password);
+  }, [email, password]);
 
-const Login = () => {
   return (
     <div id="container">
       <Header>Sleact</Header>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Label id="email-label">
           <span>이메일 주소</span>
           <div>
-            <Input type="email" id="email" name="email"/>
-          </div>
-        </Label>
-        <Label id="nickname-label">
-          <span>닉네임</span>
-          <div>
-            <Input type="text" id="nickname" name="nickname"/>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
           </div>
         </Label>
         <Label id="password-label">
           <span>비밀번호</span>
           <div>
-            <Input type="password" id="password" name="password"/>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
           </div>
+          {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
         </Label>
-        <Label id="password-check-label">
-          <span>비밀번호 확인</span>
-          <div>
-            <Input
-              type="password"
-              id="password-check"
-              name="password-check"
-            />
-          </div>
-          {/* {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
-          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {signUpError && <Error>{signUpError}</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
-        </Label>
-        <Button type="submit">회원가입</Button>
+        <Button type="submit">로그인</Button>
       </Form>
       <LinkContainer>
-        이미 회원이신가요?&nbsp;
-        <Link to="/login">로그인 하러가기</Link>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
       </LinkContainer>
     </div>
   );
 };
 
-export default Login;
+export default LogIn;
